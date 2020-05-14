98d3d25ce4571dfff8d5345ff0bc088cfb006629
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-13 18:49:41 -0700

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
-Class Kid
+class Kid
     extend FancyDance::ClassMethods
     include FancyDance::InstanceMethods
 
